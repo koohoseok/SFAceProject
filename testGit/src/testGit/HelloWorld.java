a73f0e5e28fe453ddd2ca0a0859d7a4c7213124f
Koohoseok <ha-fu-day@hanmail.net>
2017-02-13 16:18:26 +0900

    테스트 작성

--- testGit/src/testGit/HelloWorld.java
+++ testGit/src/testGit/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Modify 4:15");
+		System.out.println("테스트 작성!");
 	}
 
 }
